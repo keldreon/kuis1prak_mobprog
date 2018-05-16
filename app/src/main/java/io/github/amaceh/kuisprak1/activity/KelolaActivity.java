3b2acf38fe00f38a4a2cfc4115fe7a2d56fab5be
amaceh <achmadar9i@gmail.com>
2018-05-17 02:51:46 +0700

    folder refactor + add edit activity + basic layouting

---
+++ app/src/main/java/io/github/amaceh/kuisprak1/activity/KelolaActivity.java
@@ -0,0 +1,13 @@
+package io.github.amaceh.kuisprak1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KelolaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelola);
+    }
+}
