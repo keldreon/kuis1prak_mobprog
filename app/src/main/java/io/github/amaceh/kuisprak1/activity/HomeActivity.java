33c9d1791e445b852b6bc40bfe3886839a53cef7
Galih <galih91@student.upi.edu>
2018-05-18 05:08:04 +0700

    Daftar kontribusi: Rendy Fathagrap desain recyclerview Luvi Zatnika  desain mainActivity dan intent-intentnya Galih Abdul Muhyi backend.

--- app/src/main/java/io/github/amaceh/kuisprak1/activity/HomeActivity.java
+++ app/src/main/java/io/github/amaceh/kuisprak1/activity/HomeActivity.java
@@ -1,10 +1,11 @@
 package io.github.amaceh.kuisprak1.activity;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import io.github.amaceh.kuisprak1.R;
 
@@ -14,13 +15,29 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button bt_tambah = findViewById(R.id.bt_tambah);
-        bt_tambah.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getBaseContext(), KelolaActivity.class);
-                startActivity(i);
-            }
-        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.appbar_1);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.refresh:
+                //nanti diisi
+                return true;
+            case R.id.tambah:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
